b680b66637fe5b70b57ff771af77386c669e8b97
Wilmoth <wilmoth.kaori@gmail.com>
2018-11-05 02:45:59 +0000

    study controllerの中で、スタート画面と問題画面切り分けができるようにした

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -1,7 +1,13 @@
 class StudyController < ApplicationController
     # ユーザがログインしていないとできない
     before_action :authenticate_user!, only: [:phonics, :words, :listening, :game]
-    def phonics 
+    def phonics
+        if params.key?(:page)
+            logger.debug "params==> page:" + params[:page]
+            logger.debug "params==> num:" + params[:num]
+        end
+        @histories = History.all
         render template: 'study/phonics'
     end
+
 end
