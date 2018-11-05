15bf316e843fb2aaa3f7f07991b1c61535322af0
Wilmoth <wilmoth.kaori@gmail.com>
2018-11-05 02:09:52 +0000

    ユーザがログインしていないとできない処理をstudy controllerへ追加. deviseのサインイン時の画面遷移を決定。

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -1,4 +1,6 @@
 class StudyController < ApplicationController
+    # ユーザがログインしていないとできない
+    before_action :authenticate_user!, only: [:phonics, :words, :listening, :game]
     def phonics 
         render template: 'study/phonics'
     end
