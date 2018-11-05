af910abf983aad6dffb3345939eb70e7660ed883
Wilmoth <wilmoth.kaori@gmail.com>
2018-11-05 03:23:28 +0000

    study controllerの@phonic = PhoneticTest.offset(0).firstを追加。音声問題をデータベースから取り出して、再生できるようにした

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -6,8 +6,17 @@ class StudyController < ApplicationController
             logger.debug "params==> page:" + params[:page]
             logger.debug "params==> num:" + params[:num]
         end
-        @histories = History.all
-        render template: 'study/phonics'
+        
+        if params.key?(:page)
+            # スタディ画面か最後の画面
+            @phonic = PhoneticTest.offset(0).first
+            render template: 'study/phonics'
+        else
+            @histories = History.all
+            # 最初（履歴）の画面
+            render template: 'study/phonics_first'
+        end
+
     end
 
 end
