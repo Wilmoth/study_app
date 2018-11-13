fc3aeeaebb01939d148cfbd6d4ad254602674767
Wilmoth <wilmoth.kaori@gmail.com>
2018-11-13 07:52:24 +0000

    Phonicsナンバリング

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -2,7 +2,9 @@ class StudyController < ApplicationController
     # ユーザがログインしていないとできない
     before_action :authenticate_user!, only: [:phonics, :words, :listening, :game]
     def phonics
+        
         if params.key?(:page)
+            
             logger.debug "params==> page:" + params[:page]
             logger.debug "params==> num:" + params[:num]
         end
@@ -10,10 +12,12 @@ class StudyController < ApplicationController
         if params.key?(:page)
             # スタディ画面か最後の画面
             @phonic = PhoneticTest.offset(0).first
+            @num = params[:num].to_i + 1
             render template: 'study/phonics'
         else
             @histories = History.all
             # 最初（履歴）の画面
+            
             render template: 'study/phonics_first'
         end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get 'home/index'
-  post 'study/phonics'
+  #post 'study/phonics'
+  get 'study/phonics', to: 'study#phonics'
   resources :histories
   resources :phonetic_tests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
