5bd824a62bdafaae058aeed30013ae56451a5966
Wilmoth <wilmoth.kaori@gmail.com>
2018-11-13 08:56:53 +0000

    エンドページ追加

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -10,10 +10,22 @@ class StudyController < ApplicationController
         end
         
         if params.key?(:page)
-            # スタディ画面か最後の画面
-            @phonic = PhoneticTest.offset(0).first
-            @num = params[:num].to_i + 1
-            render template: 'study/phonics'
+            num = params[:num].to_i
+            count = PhoneticTest.count
+            if count == num 
+                #Phonicsの問題がない時
+                render template: 'study/phonics_end'
+            else #Phonics のデータがある時
+                if params.key?(:answer)
+                    @num = num + 1 #答えがあればインクリメント
+                    @message = ""
+                else
+                    @num = num
+                    @message = "こたえ　を　えらんでね"
+                end
+                @phonic = PhoneticTest.offset(num - 1).first
+                render template: 'study/phonics'
+            end
         else
             @histories = History.all
             # 最初（履歴）の画面
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   get 'home/index'
   #post 'study/phonics'
+  post 'study/phonics', to: 'study#phonics'
   get 'study/phonics', to: 'study#phonics'
   resources :histories
   resources :phonetic_tests
