c7cd124f43a299437548c1c7c7508bead8de4005
Wilmoth <wilmoth.kaori@gmail.com>
2018-11-20 09:10:14 +0000

    履歴表示、ラジオボタン、スコア表示ページ

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -10,29 +10,62 @@ class StudyController < ApplicationController
         end
         
         if params.key?(:page)
-            num = params[:num].to_i
-            count = PhoneticTest.count
-            if count == num 
-                #Phonicsの問題がない時
-                render template: 'study/phonics_end'
-            else #Phonics のデータがある時
-                if params.key?(:answer)
-                    @num = num + 1 #答えがあればインクリメント
-                    @message = ""
+            if PhoneticTest.count <= params[:num].to_i 
+                #最後の問題の時
+                num = incl_num
+                if @message.empty?
+                    #答えがあればインクリメントスコア計算
+                    score = 100
+                    #Phonicsの問題がない時
+                    h = History.new {|d|
+                      d.user_name = current_user.email
+                      d.study_date = Date.today
+                      d.score = score
+                      d.section = 1
+                      
+                    }
+                    h.save
+                    @score = score
+                    render template: 'study/phonics_end'
                 else
-                    @num = num
-                    @message = "こたえ　を　えらんでね"
+                    #　問題の番号でDBテーブルから１件取得する
+                    phonics_question(num - 1)
+                    render template: 'study/phonics'
                 end
-                @phonic = PhoneticTest.offset(num - 1).first
+            else #Phonics のデータがある時
+                num = incl_num
+                #　問題の番号でDBテーブルから１件取得する
+                phonics_question(num - 1)
                 render template: 'study/phonics'
             end
         else
-            @histories = History.all
+            @histories = History.where(user_name: current_user.email).order("created_at desc").limit(5)
             # 最初（履歴）の画面
             
             render template: 'study/phonics_first'
         end
 
+    end
+    
+    private
+    
+    def phonics_question(num)
+        @phonic = PhoneticTest.offset(num).first
+    end
+    
+    
+    def incl_num
+        # 問題の番号を変数に代入
+        num = params[:num].to_i
+        # 答えがあるかどうかを確認する
+        if params.key?(:answer)
+            @num = num + 1 #答えがあればインクリメント
+            @message = ""
+        else
+            @num = num
+            @message = "こたえ　を　えらんでね"
+        end
+        num #インクリメントしたnumを返す
     end
 
 end
