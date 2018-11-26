a84c0c03cb265805c390a42c7dd51408c07c92aa
Wilmoth <wilmoth.kaori@gmail.com>
2018-11-26 01:06:37 +0000

    スコア計算

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -2,7 +2,9 @@ class StudyController < ApplicationController
     # ユーザがログインしていないとできない
     before_action :authenticate_user!, only: [:phonics, :words, :listening, :game]
     def phonics
-        
+        if session[:answer]
+            logger.debug "回答セッション内容:" + session[:answer]
+        end
         if params.key?(:page)
             
             logger.debug "params==> page:" + params[:page]
@@ -15,7 +17,7 @@ class StudyController < ApplicationController
                 num = incl_num
                 if @message.empty?
                     #答えがあればインクリメントスコア計算
-                    score = 100
+                    score = calc_score(session[:answer])
                     #Phonicsの問題がない時
                     h = History.new {|d|
                       d.user_name = current_user.email
@@ -39,6 +41,8 @@ class StudyController < ApplicationController
                 render template: 'study/phonics'
             end
         else
+            # セッションリセット
+            session[:answer] = ''
             @histories = History.where(user_name: current_user.email).order("created_at desc").limit(5)
             # 最初（履歴）の画面
             
@@ -61,11 +65,29 @@ class StudyController < ApplicationController
         if params.key?(:answer)
             @num = num + 1 #答えがあればインクリメント
             @message = ""
+            # 答えが正解の時その番号をセッションに追記保存
+            if params[:correct] == "1" && params[:answer] == "first" \
+            || params[:correct] == "2" && params[:answer] == "second" \
+            || params[:correct] == "3" && params[:answer] == "third" 
+                session[:answer] << @num.to_s + ','
+            else
+                session[:answer] << '99,'
+            end
         else
             @num = num
             @message = "こたえ　を　えらんでね"
         end
         num #インクリメントしたnumを返す
+    end
+    
+    def calc_score(str)
+        # カンマ区切りの文字列から回答数を取得 
+        count = str.split(",").count
+        # カンマ区切りの文字列から不正解数を取得 
+        incorrect = str.split(",").count("99")
+        #正解率計算
+        logger.debug "count:" + count.to_s + " incorrect:" + incorrect.to_s
+        ((count - incorrect).to_f / count.to_f * 100).to_i 
     end
 
 end
