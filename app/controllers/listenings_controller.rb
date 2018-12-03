adb465353e126194737a9fdad195e14796f44eae
Wilmoth <wilmoth.kaori@gmail.com>
2018-12-03 06:28:26 +0000

    listeningsページのバックグラウンドを変更、seedsの更新

--- app/controllers/listenings_controller.rb
+++ app/controllers/listenings_controller.rb
@@ -1,4 +1,5 @@
 class ListeningsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_listening, only: [:show, :edit, :update, :destroy]
 
   # GET /listenings
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,5 @@
 class WordsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_word, only: [:show, :edit, :update, :destroy]
 
   # GET /words
